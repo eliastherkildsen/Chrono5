ba6d2a3933ad7472ca33325043fa9053b85f10db
Elias Therkildsen <73726902+eliastherkildsen@users.noreply.github.com>
2023-11-01 17:25:38 +0100

    made the create method talk to the database.

--- src/Main.java
+++ src/Main.java
@@ -277,7 +277,6 @@ public class Main{
 
     }
 
-
     // ANSI escape code colors.
     // from -> https://stackoverflow.com/questions/5762491/how-to-print-color-in-console-using-system-out-println
     public static final String ANSI_RESET = "\u001B[0m";
